1d3267a37373a3964bdc8806a4ff1498d5eec1ca
Dmitry <3708576@gmail.com>
2022-01-26 23:10:16 +0200

    feat: all complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+   let number;
+    if (n < 0) {
+    number = (-n).toString();
+    } else {
+    number = n.toString();
+    }
+    let i = number.length;
+    let result = '';
   
+  while (i > 0) {
+     
+    result = `${result}${number[i-1]}`;
+      i--;
+  }
+  return result;
 }
